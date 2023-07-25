ad6a639545ea823dc9b34caf50e5c70fca6f66be
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-07-25 11:16:39 -0300

    Update and rename Exercicios.dart to ExerciciosAULA2.dart

---
+++ ExerciciosAULA2.dart
@@ -0,0 +1,96 @@
+//QUESTÂO 1
+import 'dart:io';
+void main(){
+//   double num1=6;
+//   double num2=5;
+  
+//   print("A soma é $num1 + $num2 = ${num1+num2}");
+  
+//    print("A subtração é $num1 - $num2 = ${num1-num2}");
+  
+  
+//   print("A multiplicação é $num1 X $num2 = ${num1*num2}");
+  
+//   print("A Divisão é $num1 / $num2 = ${num1/num2}");
+  
+//   print("O resto da divisão é $num1 % $num2 = ${num1%num2}");  
+// }
+
+//QUESTÃO 2
+
+// import 'dart:io';
+// void main() {
+// print("Digite um número");
+
+
+// double num1=double.parse(stdin.readLineSync()!);
+
+// if(num1%2==0){
+//   print("${num1} é um número par");
+// } else{
+//    print("${num1} é um número impar");
+// }
+  
+
+//QUESTÃO 3
+// print("Digite um número");
+
+
+// double num1=double.parse(stdin.readLineSync()!);
+
+// print("O número é maior que 10 e menor que 20?: ${10<num1 && 20>num1}");
+
+// print("O número é igual a 0 ou 50? : ${0==num1 || 50==num1}");
+
+// print("O número é diferente de 100  ou igual a 200? : ${100!=num1 || 200==num1}");
+
+
+//QUESTÃO 4
+
+//   print("Digite sua idade: ");
+// double idade=double.parse(stdin.readLineSync()!);
+
+// if(idade>=18){
+//   print("Você é um maior de idade");
+// } else{
+//    print("Você é um menor de idade");
+// }
+  
+
+
+//QUESTÃO 5
+//   print("Digite um número entre 0 e 8 (esses extremos não são opções) para representar os dias da semana: ");
+// int dia=int.parse(stdin.readLineSync()!);
+
+// switch (dia){
+//   case 1:
+//     print("Hoje é Domingo");
+//     break;
+
+//   case 2:
+//     print("Hoje é Segunda-Feira");
+//     break;
+
+//   case 3:
+//     print("Hoje é Terça-Feira");
+//     break;
+
+//   case 4:
+//     print("Hoje é Quarta-Feira");
+//     break;
+
+//   case 5:
+//     print("Hoje é Quinta-Feira");
+//     break;
+
+//   case 6:
+//     print("Hoje é Sexta-Feira");
+//     break;
+
+//   case 7:
+//     print("Hoje é Sábado");
+//     break;
+  
+  
+
+}
