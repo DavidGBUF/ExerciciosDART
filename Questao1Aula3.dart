9157c1b3c7d7b1a7c73848cfdf4ab9e2c8b7f1f1
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-07-27 07:30:06 -0300

    Create Questao1Aula3.dart

---
+++ Questao1Aula3.dart
@@ -0,0 +1,11 @@
+void main(){
+    print("Tempo para o lançamento: 10 segundos");
+    for(int i =10; i>-1;i--){
+      print(i);
+    }
+    print("O Foguete está voando!");
+    print("O-O que?! O foguete está vazando combustível!");
+    print("VAI EXPLODIR!");
+    print("KBOMMMM!");
+
+}
